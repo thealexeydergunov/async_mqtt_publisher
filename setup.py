dfb062b62e95830a9b03631717597831651dbd3e
alexeydergunov <adergunov@samocat.net>
2022-07-14 16:22:16 +0700

    upd

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='async_mqtt_publisher',
+    description='MQTT publisher based on aiohttp',
     version='0.0.0',
     license='MIT',
     author="Alexey Dergunov",
     author_email='dergunovalexey2000@gmail.com',
-    packages=find_packages('async_mqtt_publisher'),
-    package_dir={'': 'async_mqtt_publisher'},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     url='https://github.com/thealexeydergunov/async_mqtt_publisher.git',
     keywords='MQTT publisher based on aiohttp',
     install_requires=[
